763e6beed039790ac64eed61e3e7e34e0ac0d68a
David Ogola <Davidogola10@gmail.com>
2023-09-19 12:26:14 -0700

    get width file

--- get_width.c
+++ get_width.c
@@ -3,26 +3,26 @@
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
- * @m: List of argumentts to be printed.
+ * @i: List of arguments to be printed.
  * @list: list of arguments.
  *
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_m;
+	int curr_i;
 	int width = 0;
 
-	for (curr_m = *m + 1; format[curr_m] != '\0'; curr_m++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[curr_m]))
+		if (is_digit(format[curr_i]))
 		{
 			width *= 10;
-			width += format[curr_m] - '0';
+			width += format[curr_i] - '0';
 		}
-		else if (format[curr_m] == '*')
+		else if (format[curr_i] == '*')
 		{
-			curr_m++;
+			curr_i++;
 			width = va_arg(list, int);
 			break;
 		}
@@ -30,7 +30,7 @@ int get_width(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*m = curr_m - 1;
+	*i = curr_i - 1;
 
 	return (width);
 }

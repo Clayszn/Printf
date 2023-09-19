5fe4827a5a3635936f52de2c6546df182268e260
David Ogola <Davidogola10@gmail.com>
2023-09-19 10:41:02 -0700

    get width file

--- get_width.c
+++ get_width.c
@@ -3,7 +3,7 @@
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
- * @m: List of arguments to be printed.
+ * @m: List of argumentts to be printed.
  * @list: list of arguments.
  *
  * Return: width.
